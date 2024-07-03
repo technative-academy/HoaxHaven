5ee68e3b9c5fe17276da2b8f16a1cea7b9e89552
Kamila W <kamila.wilczynska92@gmail.com>
2024-07-03 14:43:50 +0100

    working progress
    Co-authored-by: Nico Clack <nico@bn7.net>
    Co-authored-by: Seb <Sebbybobbler@users.noreply.github.com>
    Co-authored-by: Elyas <BlueElyas@users.noreply.github.com>

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,31 +1,27 @@
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-
-// Load environment variables from a .env file
-dotenv.config();
 
 // Middleware function to authenticate a token
 const authenticateToken = (req, res, next) => {
-  // Get the authorization header from the request
-  const authHeader = req.headers["authorization"];
+	// Get the authorization header from the request
+	const authHeader = req.headers["authorization"];
 
-  // Extract the token from the header, if it exists
-  const token = authHeader && authHeader.split(" ")[1];
+	// Extract the token from the header, if it exists
+	const token = authHeader && authHeader.split(" ")[1];
 
-  // If no token is found, send a 401 (Unauthorized) response
-  if (!token) return res.sendStatus(401);
+	// If no token is found, send a 401 (Unauthorized) response
+	if (!token) return res.sendStatus(401);
 
-  // Verify the token using the secret key
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    // If verification fails, send a 403 (Forbidden) response
-    if (err) return res.sendStatus(403);
+	// Verify the token using the secret key
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+		// If verification fails, send a 403 (Forbidden) response
+		if (err) return res.sendStatus(403);
 
-    // If verification is successful, attach the user object to the request
-    req.user = user;
+		// If verification is successful, attach the user object to the request
+		req.user = user;
 
-    // Call the next middleware or route handler
-    next();
-  });
+		// Call the next middleware or route handler
+		next();
+	});
 };
 
 export default authenticateToken;
